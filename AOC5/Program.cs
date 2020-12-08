56f36490e0090b19cd9ac93ec2a9d59d75b0552b
Michiel R <demichiel@gmail.com>
2020-12-08 19:16:05 +0100

    AOC5 & AOC6

---
+++ AOC5/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AOC5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var allLines = File.ReadAllLines("/home/michiel/dev/AOC/AOC5/input.txt").ToList();
+
+            var boardingPasses = allLines.Select(x => new BoardingPass
+            {
+                Code = x.ToCharArray()
+            }).ToList();
+            
+            boardingPasses.ForEach(Console.WriteLine);
+
+            Console.WriteLine(boardingPasses.Max(x => x.SeatId));
+
+            var allSeatIds = boardingPasses.Select(x => x.SeatId).ToList();
+
+            var minSeatId = allSeatIds.Min();
+            var maxSeatId = allSeatIds.Max();
+
+            Console.WriteLine(minSeatId);
+            Console.WriteLine(maxSeatId);
+
+            for (int i = minSeatId; i <= maxSeatId; i++)
+            {
+                if (!allSeatIds.Contains(i))
+                {
+                    Console.WriteLine($"Your seat is {i}");
+                }
+            }
+        }
+    }
+
+    public class BoardingPass
+    {
+        public char[] Code { get; set; }
+
+        public string RowAsBinary
+        {
+            get
+            {
+                return new string(Code.ToList().Take(7).Select(x => x == 'F' ? '0' : '1').ToArray());
+            }
+        }
+
+        public string SeatAsBinary
+        {
+            get
+            {
+                return new(Code.ToList().Skip(7).Select(x => x == 'L' ? '0' : '1').ToArray());
+            }
+        }
+
+        public int Row => Convert.ToInt32(RowAsBinary, 2);
+
+        public int Seat => Convert.ToInt32(SeatAsBinary, 2);
+
+        public int SeatId => (Row * 8) + Seat;
+
+        public override string ToString()
+        {
+            return $"Seat {new string(Code)} is {RowAsBinary} in binary. Row {Row}, seat {Seat}, id {SeatId}";
+        }
+    }
+}
---
+++ AOC6/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AOC6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var allLines = File.ReadAllLines("/home/michiel/dev/AOC/AOC6/input.txt").ToList();
+
+            var concatGroups = new List<string>
+            {
+                ""
+            };
+
+            foreach (var line in allLines)
+            {
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    concatGroups.Add("");
+                }
+                else
+                {
+                    concatGroups[^1] = string.Concat(concatGroups[^1], line);
+                }
+            }
+
+            var allCounts = concatGroups.Select(x => x.ToCharArray().Distinct().Count());
+
+            Console.Write("Part 1: ");
+            Console.WriteLine(allCounts.Sum());
+
+            var groups = new List<List<char[]>>
+            {
+                new()
+            };
+
+            foreach (var line in allLines)
+            {
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    groups.Add(new List<char[]>());
+                }
+                else
+                {
+                    groups[^1].Add(line.ToCharArray());
+                }
+            }
+
+            var everyoneAnswers = new List<int>();
+
+            foreach (var group in groups)
+            {
+                var maxCount = group.Count();
+
+                var allAnswers = new string(group.SelectMany(x => x).ToArray());
+
+                var answerWithCount = allAnswers.GroupBy(x => x).Select(x => new
+                {
+                    Character = x.Key,
+                    Count = x.Count()
+                });
+
+                everyoneAnswers.Add(answerWithCount.Count(x => x.Count == maxCount));
+            }
+
+            var sum = 0;
+            everyoneAnswers.ForEach(a => sum += a);
+            
+            Console.Write("Part 2:");
+            Console.WriteLine(sum);
+        }
+    }
+}
